bb5ec9731a85cf1731c9ac80663ff6777957575c
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-05-07 21:29:18 +0500

    Added mycourse function

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import StudentRegisterView, StudentDashboardView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("student-dashboard",StudentDashboardView)
 
 urlpatterns = [
     path('student-register/',StudentRegisterView.as_view(), name='student-register'),
-    path('student-dashboard/',StudentDashboardView.as_view(), name='student-dashboard')
+    path('',include(router.urls)),
 ]
--- student/views.py
+++ student/views.py
@@ -1,17 +1,29 @@
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from base.models import Course
+from base.serializers import CourseSerializer
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 class StudentRegisterView(CreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = [IsAuthenticated,]
 
-class StudentDashboardView(ListAPIView):
+class StudentDashboardView(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = [IsAuthenticated,]
+
+    @action(detail=False, methods=["GET"])
+    def mycourse(self, request, *args, **kwargs):
+        student = Student.objects.get(user=request.user)
+        course = Course.objects.filter(students=student)
+        serializer = CourseSerializer(course, many=True)
+        return Response(serializer.data)
 
     def get_queryset(self):
         return Student.objects.filter(user=self.request.user)

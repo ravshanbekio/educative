173342da296da26205a153e92b117a4808ac8f59
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-05-07 21:04:07 +0500

    Added documentation

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,22 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Netflix API",
+      default_version='v1',
+      description="Netflix dasturining klon versiyasi. Bemalol kirib ishlatishingiz mumkin. API bepul!!! \n Istaganingizcha foydalaning",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact("Ravshanbek Madaminov <ravshanbekmadaminov68@gmail.com> <+998903036415>"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
